362c0184e8114df55a4944bc5078c010bc878d3c
chumeyos <jesus_chu50@hotmail.com>
2016-08-19 10:26:06 -0600

    actualizacion
    es un avance

--- src/proyecto/r/SeleccionProducto.java
+++ src/proyecto/r/SeleccionProducto.java
@@ -8,6 +8,8 @@ package proyecto.r;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.Image;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
@@ -17,18 +19,26 @@ import javax.swing.JPanel;
  *
  * @author USUARIO FINAL
  */
-public class SeleccionProducto extends JPanel {
+public class SeleccionProducto extends JPanel implements MouseListener{
+    
    
-    private final JLabel botonAñadir = new JLabel();
+    private final JLabel botonDisminuir = new JLabel();
     private final JLabel botonEliminar = new JLabel();
     private final JLabel nombre = new JLabel();
     private final JLabel producto = new JLabel();
-    private final String imagen ;
+   // private final String imagen ;
     private final JPanel botones = new JPanel();
     private final Dimension x = new Dimension(100,100);
+    private final Producto a;
     
-    public SeleccionProducto(){
+    
+    public SeleccionProducto( Producto a){
         
+       
+    
+        this.a = a;
+        
+        addMouseListener(this);
         setLayout( new BorderLayout());
         
         setPreferredSize(x);
@@ -36,20 +46,47 @@ public class SeleccionProducto extends JPanel {
         add(producto,"Center");
         add(botones, "South");
         
-        botones.add(botonAñadir,"West");
+        botones.add(botonDisminuir,"West");
         botones.add(botonEliminar,"East");
         botones.setLayout(new BorderLayout());
         
+        botonDisminuir.addMouseListener(this);
+        botonEliminar.addMouseListener(this);
         
-        imagen = " ";
+       // imagen = " ";
         
         
     }
     
     private void configurarLogo(){
-        producto.setIcon(new ImageIcon(new ImageIcon("Productos/" + imagen).getImage()
+        producto.setIcon(new ImageIcon(new ImageIcon("Productos/" + ).getImage()
                 .getScaledInstance( 50, 50, Image.SCALE_DEFAULT)));    
         
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+         //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+         //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        //To change body of generated methods, choose Tools | Templates.
     }
     
 }

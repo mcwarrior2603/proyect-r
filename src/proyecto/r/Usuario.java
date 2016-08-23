303d3bf6b26c938201be716e989d4b7d04cf8344
MCwarrior <mcwarrior.mendez@hotmail.com>
2016-08-22 18:07:22 -0600

    Fusiona

---
+++ src/proyecto/r/Usuario.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto.r;
+
+/**
+ *
+ * @author MCwar
+ */
+public class Usuario {
+    
+    private int id;
+    public String nombre;
+    public int nivelDeAcceso;
+    
+    public Usuario(int id, String nombre, int nivelDeAcceso){
+        this.id = id;
+        this.nombre = nombre;
+        this.nivelDeAcceso = nivelDeAcceso;
+    }
+    
+}
